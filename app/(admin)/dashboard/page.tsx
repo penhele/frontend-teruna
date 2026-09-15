6722535f2295d5456264ad34b22fb52ac2d76ed7
Stephen Helenus <stephenhelk@gmail.com>
2026-09-15 10:24:31 +0700

    feat: add table

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,7 +1,37 @@
+import { DataTable } from "@/components/layouts/data-table";
+import { columms } from "@/features/congregation/columns";
+
 export default function Page() {
+  const data = [
+    {
+      id: "123456",
+      name: "Stephen Helenus",
+      class: "EKA",
+      gender: "Laki-laki",
+      sektor: "4",
+      birth: "2004-08-15",
+      phone: "081234567890",
+      isActive: true,
+      isSidi: true,
+      sidiYear: 2021,
+    },
+    {
+      id: "345678",
+      name: "Ivana",
+      class: "DWI",
+      gender: "Perempuan",
+      sektor: "6",
+      birth: "2004-08-15",
+      phone: "081298765432",
+      isActive: true,
+      isSidi: true,
+      sidiYear: 2021,
+    },
+  ];
+
   return (
     <div className="">
-      <div className="">halo dunia</div>
+      <DataTable columns={columms} data={data} />
     </div>
   );
 }
