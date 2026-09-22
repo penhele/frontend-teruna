64c5763b4c4f7db6708b5937fb983dc3ff1ef069
Stephen Helenus <stephenhelk@gmail.com>
2026-09-22 23:44:08 +0700

    perbaiki space

--- components/layouts/data-table.tsx
+++ components/layouts/data-table.tsx
@@ -156,7 +156,7 @@ export function DataTable<TData extends RowData>({
         </Table>
       </div>
 
-      <div className="flex items-center justify-end space-x-2 py-4">
+      <div className="flex items-center justify-end space-x-2">
         <Button
           variant="outline"
           size="sm"
